a7bf50e7baeba63c7627d053540e35f239b8536f
mrRicochet <123089587+mrRicochet@users.noreply.github.com>
2023-02-05 17:29:48 +0700

    Add files via upload

---
+++ Zadacha_2/Zadacha_2/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+int min = numB;
+
+if (numA > numB)
+{
+    System.Console.WriteLine("max=" + max);
+    System.Console.WriteLine("min=" + min);
+}
+else 
+{
+    max = numB;
+    min = numA;
+    System.Console.WriteLine("max=" + max);
+    System.Console.WriteLine("min=" + min);
+}
