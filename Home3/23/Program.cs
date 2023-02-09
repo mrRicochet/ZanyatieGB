c80b36326f16d20de53756cc15d6893a629d3c74
mrRicochet <123089587+mrRicochet@users.noreply.github.com>
2023-02-09 21:49:20 +0700

    Add files via upload
    Практическое задание 19, 21, 23

---
+++ Home3/23/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+//Напишите программу, которая принимает 
+//на вход число (N) и выдаёт таблицу кубов 
+//чисел от 1 до N.
+
+//3 -> 1, 4, 9.
+//5 -> 1, 8, 27, 64, 125.
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine
+    (str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = InputNumber("Введите число N: ");
+
+for(int num = 1; num <= N; num++)
+{
+    System.Console.Write(Math.Pow(num, 3) + ", ");
+} 
