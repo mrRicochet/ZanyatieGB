be299ae9fb32d990a07ccfce05f6c3cdd4df378e
mrRicochet <123089587+mrRicochet@users.noreply.github.com>
2023-02-16 18:29:07 +0700

    Add files via upload
    Урок 5. Функции и одномерные массивы

---
+++ Homework5/36/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный 
+// случайными числами. Найдите сумму элементов, стоящих 
+// на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GenerateArray(int size) //Генерируем новый массив
+{
+    int[] array = new int[size]; //выделим память под массив
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(-30, 30); // задаем рандомный массив с диапазоном от -30 до 30
+    }
+    return array; // выводим массив
+}
+
+void PrinArray(int[] array) //Метод вывода массива
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int SumOddNumbers(int[] arr)
+{
+    int result = 0; //вводим переменную
+    for (int i = 0; i < arr.Length; i++) //Прогоняем все элементы c нечетными индексами
+    {
+        if (i % 2 != 0) //Если остаток от деления не равен 0, то индекс нечетный
+        {
+            result = result + arr[i]; //Кладём его в переменную
+        }
+    }
+    return result; // возвращаем результат
+}
+
+int[] numArr = GenerateArray(15); //Вызываем новый массив из 15 элементов
+PrinArray(numArr); // выводим массив
+int sumOdd = SumOddNumbers(numArr); //вызываем функцию счета чисел с нечетными индексами 
+System.Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях = {sumOdd}"); // выводим результат на экран
---
+++ Homework5/38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите 
+// разницу между максимальным и минимальным 
+// элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] GenerateArray(int size) //Генерируем новый массив вещественных чисел
+{
+    double[] array = new double[size]; //выделим память под массив
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Math.Round((random.NextDouble()) * 100, 1); //задаем рандомный массив (умножаем на 100, чтобы числа были от 0 до 100)
+    }
+    return array; // выводим массив
+}
+
+void PrinArray(double[] array) //Метод вывода массива
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+double Difference(double[] array) // метод вычисления разницы между максимальным и минимальным значениями массива
+{
+    double difference = (array.Max()) - (array.Min());
+    return difference;
+}
+
+
+double[] numArr = GenerateArray(15); //Вызываем новый массив из 15 элементов
+PrinArray(numArr); // выводим массив
+double diff = Difference(numArr);
+System.Console.WriteLine($"Разница между максимальным и минимальным значениями = {diff}");
